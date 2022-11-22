d2215b10d61c60dd04c80f985d76aca881bcc72c
Actaer2001 <adnancrnovrsanin48@gmail.com>
2022-11-22 02:29:03 +0100

    AC-3 Dodavanje CRUD funkcionalnosti API-ju

--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,30 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Events;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class EventsController : BaseApiController
     {
-        private readonly DataContext _context;
-        public EventsController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Event>>> GetEvents() {
-            return await _context.Events.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // single Event
         public async Task<ActionResult<Event>> GetEvent(Guid id) {
-            return await _context.Events.FindAsync(id);
+            return await Mediator.Send(new Details.Query{ Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent(Event newEvent) {
+            return Ok(await Mediator.Send(new Create.Command{ Event = newEvent }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditEvent(Guid id, Event newEvent) {
+            newEvent.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{ Event = newEvent }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent(Guid id) {
+            return Ok(await Mediator.Send(new Delete.Command{ Id = id }));
         }
     }
 }
