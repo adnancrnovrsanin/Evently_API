5d82f097e59a7f012366760c3ed545b77c17208e
Actaer2001 <adnancrnovrsanin48@gmail.com>
2022-12-24 12:51:37 +0100

    Zavrsena implementacija komentara

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -33,8 +33,10 @@ namespace API.Extensions
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
-                opt.Events = new JwtBearerEvents{
-                    OnMessageReceived = context => {
+                opt.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context => 
+                    {
                         var accessToken = context.Request.Query["access_token"];
                         var path = context.HttpContext.Request.Path;
                         if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat"))) {
